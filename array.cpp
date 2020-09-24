15f6ce74a62b393c1d3b61b8806b4abb6bde8982
HimangshuB <54852489+Himangshu1086@users.noreply.github.com>
2020-09-24 10:43:21 +0530

    Add files via upload

---
+++ array.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+	int bbucky[6] = {66, 97, 87, 76};
+	for (int i = 0; i < 6; i++)
+		cout << bbucky[i] << " ";
+
+}
---
+++ friends.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class StankFist {
+
+public:
+	StankFist() {
+		stankvariable = 0;
+	}
+private:
+	int stankvariable;
+
+	// DECLARATION OF FRIEND
+
+	friend void StankFriend(StankFist &sfo);
+};
+
+void StankFriend(StankFist &sfo)
+{
+	sfo.stankvariable = 99;
+	cout << sfo.stankvariable << endl;
+}
+
+
+int main(int argc, char const *argv[])
+{
+	StankFist bob;
+	StankFriend(bob);
+	return 0;
+}
