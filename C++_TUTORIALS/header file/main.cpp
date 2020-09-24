8029c4ad733420df16ff8afc091fbd150bd189c0
HimangshuB <54852489+Himangshu1086@users.noreply.github.com>
2020-09-24 10:55:16 +0530

    Add files via upload

---
+++ C++_TUTORIALS/header file/main.cpp
@@ -0,0 +1,12 @@
+
+#include<bits/stdc++.h>
+
+#include "separateClass.h"
+
+using namespace std;
+
+int main() {
+
+	separateClass class1;
+	return 0;
+}
---
+++ C++_TUTORIALS/header file/separateClass.cpp
@@ -0,0 +1,9 @@
+#include "separateClass.h"
+#include<bits/stdc++.h>
+using namespace std;
+
+separateClass :: separateClass()
+
+{
+	cout < "hello eveyone" << endl;
+}
