66d18972c368bb492588c110207a977f71417201
HimangshuB <54852489+Himangshu1086@users.noreply.github.com>
2020-09-24 10:54:04 +0530

    Add files via upload

---
+++ C++_TUTORIALS/abc.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Mother {
+public:
+	Mother();
+	void sayName();
+
+};
+
+
+
+Mother::Mother() {
+
+}
+
+void Mother::sayName() {
+	cout << "hey you himangshu!!" << endl;
+
+}
