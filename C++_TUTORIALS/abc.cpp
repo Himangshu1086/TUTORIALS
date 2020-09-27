c2678c35acd9a515420d74f9bb635a77b9263302
Himangshu1086 <baishyahimangshu499@gmail.com>
2020-09-27 12:22:29 +0530

    update

--- C++_TUTORIALS/abc.cpp
+++ C++_TUTORIALS/abc.cpp
@@ -1,20 +1,45 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-class Mother {
-public:
-	Mother();
-	void sayName();
-
-};
+int main()
+{
 
 
 
-Mother::Mother() {
+	deque<int> dq;
+	dq.push_back(1); // {1}
+	dq.emplace_back(2); // {1, 2}
+	dq.push_front(4); // {4, 1, 2}
+	dq.emplace_front(3); // {3, 4, 1, 2}
+
+	dq.pop_back(); // {3, 4, 1}
+	dq.pop_front(); // {4, 1}
+
+	cout << dq.back() << " ";
+
+
+	cout <<	dq.front() << " ";
+
+
+	for (auto i : dq)
+	{
+		cout << i << " ";
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
 
-void Mother::sayName() {
-	cout << "hey you himangshu!!" << endl;
 
-}
+
