1a8f840268c2572b3e0af39da63526c83d767685
Himangshu1086 <baishyahimangshu499@gmail.com>
2020-09-27 12:17:05 +0530

    update

---
+++ C++_TUTORIALS/header file/practice.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+//#ifndef ONLINE_JUDGE
+//	freopen("input.txt", "r", stdin);
+//	freopen("output.txt", "w", stdout);
+//#endif
+
+
+	vector<int>v(6);
+	for (int i = 0; i < 6; i++)
+	{
+		cout << "enter the " << i << "element " << endl;
+		cin >> v[i];
+	}
+	for (int i = 0 ; i < 6; i++)
+	{
+		cout << v[i] << " ";
+	}
+
+
+	int x, y;
+	cout << "enter x and y :" << endl;
+	cin >> x >> y;
+
+}
