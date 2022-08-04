004461da11d30eba724a4928f789a4aa5275a8b6
Himangshu1086 <baishyahimangshu499@gmail.com>
2022-08-04 11:55:39 +0530

    update

--- DATA_STRUCTURE/SORTING/5__quicksort.cpp
+++ DATA_STRUCTURE/SORTING/5__quicksort.cpp
@@ -1,5 +1,36 @@
 /*
 
+The basic idea is to make the left array smaller than the pivot and right array greater than the pivot. 
+Take the first element as the pivot 
+loop from left-> right and find a element which is larger than the pivot (i)
+similar loop rom right->left and find a element which is smaller than the pivot.(j)
+than swap (i ,j )
+Repeat this till l<r
+After that swap(pivot , arr[j])
+
+i			  j
+4 6 2 5 7 9 1 3
+p
+
+  i			  j
+4 6 2 5 7 9 1 3
+
+  i			  j
+4 1 2 5 7 9 1 6   ===> swap(arr[i] , arr[j])
+
+  	  i		j
+4 3 2 5 7 9 1 6
+
+  	  i		j
+4 3 2 1 7 9 5 6   ===> swap(arr[i] , arr[j])
+
+  	  j	i	
+4 3 2 1 7 9 5 6
+
+j        
+1 3 2 4 7 9 5 6   =====> swap(arr[j] , pivot)
+	  p
+
 
 
 */
